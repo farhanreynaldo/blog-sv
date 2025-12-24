e5aebcf9514c0d0b002a9373927ba8e4cf497f8c
Farhan Reynaldo <farhanreynaldo@gmail.com>
2025-12-24 09:25:20 +0700

    Format date and writing section

--- src/routes/writing/+page.svelte
+++ src/routes/writing/+page.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
 	let { data } = $props();
+	import { formatDate } from '$lib/utils/DateFormatter';
 </script>
 
 <svelte:head>
@@ -8,9 +9,43 @@
 
 <ul>
 	{#each data.posts as post}
-		<li>
-			<p><a href={post.path}>{post.meta.title}</a></p>
-			Published on {post.meta.date}
-		</li>
+		<a href={post.path}>
+			<li>
+				<span>{post.meta.title}</span>
+				<span>{formatDate(post.meta.date)}</span>
+			</li>
+		</a>
 	{/each}
 </ul>
+
+<style>
+	ul {
+		list-style: none;
+		padding: 0;
+		margin: 0;
+		display: flex;
+		flex-direction: column;
+		gap: 1rem;
+	}
+
+	a {
+		text-decoration: none;
+		border-radius: 10px;
+
+		&:hover {
+			background-color: oklch(90% 0% 0);
+		}
+	}
+
+	li {
+		display: flex;
+		flex-direction: column;
+		color: oklch(70% 0% 0);
+		padding: 0.5rem;
+	}
+
+	span:first-child {
+		font-weight: bold;
+		color: oklch(10% 0% 0);
+	}
+</style>
--- src/routes/writing/[slug]/+page.svelte
+++ src/routes/writing/[slug]/+page.svelte
@@ -1,7 +1,27 @@
-<script>
+<script lang="ts">
 	let { data } = $props();
+
+	import { formatDate } from '$lib/utils/DateFormatter';
 </script>
 
-<h1>{data.title}</h1>
-<p>Published: {data.date}</p>
+<div class="metadata">
+	<h1>{data.title}</h1>
+	<p>{formatDate(data.date)}</p>
+</div>
 {@render data.content()}
+
+<style>
+	h1 {
+		margin: 0;
+	}
+
+	p {
+		margin: 0 0 1rem 0;
+		color: oklch(70% 0% 0);
+	}
+	.metadata {
+		display: flex;
+		flex-direction: column;
+		border-bottom: 1px solid oklch(90% 0% 0);
+	}
+</style>
