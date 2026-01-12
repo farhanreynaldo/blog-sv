969f24857b038ba9f0cd0d3024d91299c1b6ab8a
Farhan Reynaldo <farhanreynaldo@gmail.com>
2026-01-12 17:43:54 +0700

    Add placeholder for photos and readings

---
+++ src/routes/photos/+page.svelte
@@ -0,0 +1,6 @@
+<script>
+	// optional: load data here
+</script>
+
+<h1>Photos</h1>
+<p>Placeholder page for /photos</p>
---
+++ src/routes/readings/+page.svelte
@@ -0,0 +1,6 @@
+<script>
+	// optional: load data here
+</script>
+
+<h1>Readings</h1>
+<p>Placeholder page for /readings</p>
